ca433dbc4cc1deea7c29dec1624f1b5cb2cd353d
KSM-16 <khadiza.sarwar.moury@gmail.com>
2025-11-08 15:18:03 -0500

    run adamw-resnet18

--- main.py
+++ main.py
@@ -317,16 +317,16 @@ if __name__ == '__main__':
         #     'resnet34': {'label': 'ResNet34', 'labeled_sizes': [], 'accuracies': []},
         #     'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
         # },
-        # 'adamw': {
-        #          'resnet18': {'label': 'ResNet18', 'labeled_sizes': [], 'accuracies': []},
+         'adamw': {
+                  'resnet18': {'label': 'ResNet18', 'labeled_sizes': [], 'accuracies': []},
         #          'resnet34': {'label': 'ResNet34', 'labeled_sizes': [], 'accuracies': []},
         #          'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
-        # },
-         'rmsprop': {
+         },
+        # 'rmsprop': {
         #          'resnet18': {'label': 'ResNet18', 'labeled_sizes': [], 'accuracies': []},
         #          'resnet34': {'label': 'ResNet34', 'labeled_sizes': [], 'accuracies': []},
-                  'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
-         }
+        #         'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
+         #}
     }
 
     OPTIMIZER_MAP = {
