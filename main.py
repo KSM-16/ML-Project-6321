c074bc23b921e15d1a848932ae5ffc10ede96275
KSM-16 <khadiza.sarwar.moury@gmail.com>
2025-11-17 17:37:53 -0500

    run rsnet50-adamw

--- main.py
+++ main.py
@@ -318,9 +318,9 @@ if __name__ == '__main__':
           #     'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
        #  },
           'adamw': {
-                 'resnet18': {'label': 'ResNet18', 'labeled_sizes': [], 'accuracies': []},
-         #          'resnet34': {'label': 'ResNet34', 'labeled_sizes': [], 'accuracies': []},
-         #a          'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
+         #        'resnet18': {'label': 'ResNet18', 'labeled_sizes': [], 'accuracies': []},
+         #        'resnet34': {'label': 'ResNet34', 'labeled_sizes': [], 'accuracies': []},
+                   'resnet50': {'label': 'ResNet50', 'labeled_sizes': [], 'accuracies': []},
           },
         # 'rmsprop': {
          #         'resnet18': {'label': 'ResNet18', 'labeled_sizes': [], 'accuracies': []},
